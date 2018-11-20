b6cfc2646ba7a0360c14b36c4bb308ddfc2925fe
clam3154 <clam3154@bths.edu>
2018-11-20 09:54:58 -0500

    so far

--- src/mergeSort.java
+++ src/mergeSort.java
@@ -1,4 +1,23 @@
 public class mergeSort
 {
+    public void mergeSort(int[] arr)
+    {
+        int n = arr.length;
+        int[] temp = new int[n];
+        mergeSortHelper(arr,0,n-1,temp);
+    }
+    public void mergeSortHelper(int[] arr, int left, int right, int[] temp)
+    {
+        if(left<right)
+        {
+            int mid = (left+right)/2;
+            mergeSortHelper(arr,left,mid,temp);
+            mergeSortHelper(arr,left,mid+1,temp);
+            merge(arr,left,mid,right,temp);
+        }
+    }
+    public void merge(int[] arr, int left, int mid, int right, int[] temp)
+    {
 
+    }
 }
