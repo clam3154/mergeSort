acecafe5f381e344964202c8d29c3fccd7db7aa9
clam3154 <clam3154@bths.edu>
2018-11-21 09:20:00 -0500

    done

--- src/mergeSort.java
+++ src/mergeSort.java
@@ -18,6 +18,38 @@ public class mergeSort
     }
     public void merge(int[] arr, int left, int mid, int right, int[] temp)
     {
-
+        int i = left;
+        int j = mid+1;
+        int k = left;
+        while(i <= mid+1 && j <= right)
+        {
+            if(arr[i] < arr[j])
+            {
+                temp[k] = arr[i];
+                i++;
+            }
+            else
+            {
+                temp[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+        while(i <= mid)
+        {
+            temp[k] = arr[i];
+            i++;
+            k++;
+        }
+        while(j <= right)
+        {
+            temp[k] = arr[j];
+            j++;
+            k++;
+        }
+        for(int x = left; x <= right; x++)
+        {
+            arr[x] = temp[x];
+        }
     }
 }
